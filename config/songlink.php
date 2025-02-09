3d8d0c1e6f61262bae1af3a00b30b0c946e85609
Grayson Erhard <develop@graysonerhard.com>
2025-02-09 14:11:50 -0700

    Update config file name and remove default platforms section

--- config/songlink.php
+++ config/songlink.php
@@ -1,6 +1,6 @@
 <?php
 
-// config/odesli.php
+// config/songlink.php
 
 return [
 
@@ -13,21 +13,4 @@ return [
     |
     */
     'api_url' => env('SONGLINK_API_URL', 'https://api.song.link/v1-alpha.1/links'),
-
-    /*
-    |--------------------------------------------------------------------------
-    | Default Platforms
-    |--------------------------------------------------------------------------
-    |
-    | You can list the platforms you are interested in. In the API response
-    | the links are grouped by platform.
-    |
-    */
-    'default_platforms' => [
-        'spotify',
-        'appleMusic',
-        'youtube',
-        'deezer',
-        // add more as needed...
-    ],
 ];
