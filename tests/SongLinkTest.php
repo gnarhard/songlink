ad9bdbe740cceb3e5f8ed84d857312e7f263ea36
gnarhard <7062755+gnarhard@users.noreply.github.com>
2025-02-09 21:04:13 +0000

    Fix styling

--- tests/SongLinkTest.php
+++ tests/SongLinkTest.php
@@ -1,8 +1,8 @@
 <?php
 
+use Gnarhard\SongLink\Facades\SongLink as SongLinkFacade;
 use Gnarhard\SongLink\Models\SongLink;
 use Illuminate\Support\Facades\Artisan;
-use Gnarhard\SongLink\Facades\SongLink as SongLinkFacade;
 
 it('can fetch song links', function () {
     $spotifyUrl = 'https://open.spotify.com/track/4hhbLHdsBw4y0AR9iBV0CN?si=7086c6871b9c49a1';
@@ -23,9 +23,9 @@ it('can store song links', function () {
 
     Artisan::call('songlink:store', [
         'spotifyUrl' => $spotifyUrl,
-        'title'      => $songTitle,
-        'slug'       => $slug,
-        'isSingle'   => true,
+        'title' => $songTitle,
+        'slug' => $slug,
+        'isSingle' => true,
     ]);
 
     expect(SongLink::count())->toBe(1);
