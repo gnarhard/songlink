282b8bf21e7f30832af34967f8d804a1e3e2804b
gnarhard <7062755+gnarhard@users.noreply.github.com>
2025-02-28 22:04:02 +0000

    Fix styling

--- src/Http/Controllers/SongLinkController.php
+++ src/Http/Controllers/SongLinkController.php
@@ -20,14 +20,14 @@ class SongLinkController
             ?? SongLink::select('slug', 'created_at')->latest()->value('slug');
 
         // $song = cache()->remember("song_{$songSlug}", now()->addYear(), function () use ($songSlug) {
-            $song = SongLink::where('slug', $songSlug)->first();
-            // if ($song == null) {
-            //     return null;
-            // }
-            $song->common_platform_urls = FacadesSongLink::getPlatformUrls($song->links, true);
-            $song->uncommon_platform_urls = FacadesSongLink::getPlatformUrls($song->links, false);
+        $song = SongLink::where('slug', $songSlug)->first();
+        // if ($song == null) {
+        //     return null;
+        // }
+        $song->common_platform_urls = FacadesSongLink::getPlatformUrls($song->links, true);
+        $song->uncommon_platform_urls = FacadesSongLink::getPlatformUrls($song->links, false);
 
-            // return $song;
+        // return $song;
         // });
 
         if ($song == null) {
