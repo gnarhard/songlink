5d7f9dfa5a498974c51ee89170423ac411c49167
Grayson Erhard <develop@graysonerhard.com>
2025-02-28 15:05:40 -0700

    Enable caching logic in SongLinkController

--- src/Http/Controllers/SongLinkController.php
+++ src/Http/Controllers/SongLinkController.php
@@ -19,16 +19,16 @@ class SongLinkController
             ?? $request->query('t')
             ?? SongLink::select('slug', 'created_at')->latest()->value('slug');
 
-        // $song = cache()->remember("song_{$songSlug}", now()->addYear(), function () use ($songSlug) {
+        $song = cache()->remember("song_{$songSlug}", now()->addYear(), function () use ($songSlug) {
             $song = SongLink::where('slug', $songSlug)->first();
-            // if ($song == null) {
-            //     return null;
-            // }
+            if ($song == null) {
+                return null;
+            }
             $song->common_platform_urls = FacadesSongLink::getPlatformUrls($song->links, true);
             $song->uncommon_platform_urls = FacadesSongLink::getPlatformUrls($song->links, false);
 
-            // return $song;
-        // });
+            return $song;
+        });
 
         if ($song == null) {
             return view('errors.404');
