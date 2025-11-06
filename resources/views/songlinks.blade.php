a55e03e0e40086ab1dcac7a783d7b2c77cd053a3
Grayson Erhard <develop@graysonerhard.com>
2025-11-06 13:29:16 -0700

    fix: remove max-width

--- resources/views/songlinks.blade.php
+++ resources/views/songlinks.blade.php
@@ -8,7 +8,7 @@
 
 <div style="background: url({{ asset($backgroundImage ?? $song->album_artwork_path) }}); background-size: cover; background-position: center;" class="container-fluid">
     <div class="row" style="background-color: rgba(0,0,0,.7);">
-        <div class="col col-md-8 mx-auto py-4" style="max-width: 600px;">
+        <div class="col col-md-8 mx-auto py-4">
             <h6 class="text-center mb-1 d-block">LISTEN TO</h6>
             <h2 class="text-center text-uppercase mb-0">{{ $song->title }}</h2>
             <h4 class="text-center mb-4">{{ $artist ?? '' }}</h4>
